9183087dc23e60fe94670723137b9a56661a2d41
Lua <xdaddisxd@gmail.com>
2022-03-21 10:06:17 -0400

    update routes

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,6 @@ import { Dashboard } from './pages/dashboard';
 import { Landing } from './pages/landing';
 import { Home } from './pages/home';
 import { Form } from './pages/form';
-import { Form1 } from './pages/form1';
-import { Form2 } from './pages/form2';
-import { Form3 } from './pages/form3';
 import { Form4 } from './pages/form4';
 
 ReactDOM.render(
@@ -31,18 +28,6 @@ ReactDOM.render(
         <Route
           path="/form"
           element={<Form />}
-        />
-        <Route
-          path="/form1"
-          element={<Form1 />}
-        />
-        <Route
-          path="/form2"
-          element={<Form2 />}
-        />
-        <Route
-          path="/form3"
-          element={<Form3 />}
         />
         <Route
           path="/form4"
