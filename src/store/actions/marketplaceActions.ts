aef5de0d2c58fc51b551ea9b68e64750f3c58a57
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-03-22 14:34:41 -0400

    delete console.log

--- src/store/actions/marketplaceActions.ts
+++ src/store/actions/marketplaceActions.ts
@@ -3,7 +3,7 @@ import { initialMarketplace } from '../reducers/marketplaceReducers';
 import * as TYPES from '../types';
 
 export const setBusinessDetails = (businessDetails: typeof initialMarketplace.businessDetails) => {
-  console.log(businessDetails);
+  console.debug(businessDetails);
   return (dispatch : Dispatch) => {
     dispatch({
       type: TYPES.SET_BUSINESS_DETAILS,
@@ -13,7 +13,7 @@ export const setBusinessDetails = (businessDetails: typeof initialMarketplace.bu
 };
 
 export const resetMarketplace = () => {
-  console.log();
+  console.debug();
   return (dispatch : Dispatch) => {
     dispatch({
       type: TYPES.RESET_MARKETPLACE,
