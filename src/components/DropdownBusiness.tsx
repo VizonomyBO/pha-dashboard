b9e068ad96f979335b7ffdb0016ec097821a3574
Lua <xdaddisxd@gmail.com>
2022-03-29 17:53:05 -0400

    move logic to hook

--- src/components/DropdownBusiness.tsx
+++ src/components/DropdownBusiness.tsx
@@ -1,39 +1,20 @@
-import { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import { DEFAULT_DROPDOWN_OPTION, STATES } from '../constants';
+import { useDropdownBusiness } from '../store/hooks/custom/useDropdownBusiness';
 import { DropdowInterface } from '../@types';
-import { HOURS } from '../constants/hours';
-import { useMarketplaceDispatch } from '../store/hooks/marketplaceHook';
 
 export const DropdownBusiness = ({ initialState, type } : DropdowInterface) => {
-  const { setBusinessDetails } = useMarketplaceDispatch();
-  const [option, setOption] = useState('States');
-  const [options, setOptions] = useState<string[]>([]);
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const handleMenuItemClick = (index: number) => {
-    setOption(options[index]);
-    setBusinessDetails(type, options[index]);
-    handleClose();
-  };
-  useEffect(() => {
-    setOption(initialState);
-    if (initialState === DEFAULT_DROPDOWN_OPTION.STATES) {
-      setOptions(STATES);
-    }
-    if (initialState === DEFAULT_DROPDOWN_OPTION.CLOSE || initialState === DEFAULT_DROPDOWN_OPTION.OPEN) {
-      setOptions(HOURS);
-    }
-  }, [initialState]);
+  const {
+    handleClick,
+    option,
+    options,
+    open,
+    anchorEl,
+    handleClose,
+    handleMenuItemClick
+  } = useDropdownBusiness({ initialState, type });
   return (
     <div className="ainput2">
       <Button
@@ -47,7 +28,7 @@ export const DropdownBusiness = ({ initialState, type } : DropdowInterface) => {
       </Button>
       <Menu
         id="dropdown-business"
-        anchorEl={anchorEl}
+        anchorEl={anchorEl as unknown as Element}
         open={open}
         onClose={handleClose}
       >
