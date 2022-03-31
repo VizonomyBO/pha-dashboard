9653ff8e8254ab121ebad6452638009ae8637a3e
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-03-31 17:14:12 -0400

    delete console log and add VISIBLE_TIME

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -2,6 +2,7 @@ export const NO_DATA = 'no-data';
 export const BUSINESS_DETAILS = 'businessDetails';
 export const OTHER_QUESTIONS = 'otherQuestions';
 export const CONTACT_DETAILS = 'contactDetails';
+export const VISIBLE_TIME = 2000;
 export const MAPBOX_KEY = process.env.REACT_APP_MAPBOX_KEY || NO_DATA;
 export const BASEMAP = 'mapbox://styles/vizonomy/cl0zk8u1o002w15pvqr3t0e1b';
 export const CLASSES_BY_FORM = {
