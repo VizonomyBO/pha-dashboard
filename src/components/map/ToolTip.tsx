1c21e3fab4ed1fc852434b7707aac82a12cca8ac
Edav <edav13@hotmail.com>
2022-03-31 11:03:08 -0400

    change desing tooltip

--- src/components/map/ToolTip.tsx
+++ src/components/map/ToolTip.tsx
@@ -3,61 +3,33 @@ import React from 'react';
 export const ToolTip = (data: { x: number, y: number }) => {
   const { x, y } = data;
   return (
-    <div className="bmodal" style={{ left: x, top: y }}>
+    <div className="modal" style={{ left: x, top: (y - 460) }}>
       <figure className="picture">
-        <img src="/public/images/img_market_s_001.png" alt="" />
+        <img src="/public/images/img_market_001.png" alt="" />
       </figure>
       <div className="detailcard">
         <div className="store">JABBO’S COMPACT MARKET</div>
-        <div className="attribute">
-          <span className="aitem">Fresh</span>
-          <span className="aitem">Quality</span>
-          <span className="aitem">Visible</span>
+        <div className="services">
+          <span className="kind icmkdish" />
+          <span className="kind icmkshop" />
+          <span className="kind icmkdish" />
+          <span className="kind icmkshop" />
+          <span className="kind icmkshop" />
         </div>
-        <div className="address">923 Ohio Ave, Clarksdale, MS</div>
+        <div className="address">923 Ohio Ave, Clarksdale, MS 38614</div>
         <div className="phone">
           <span className="icphone" />
           <span className="number">(704) 969-9650</span>
         </div>
+        <button className="light" type="button">
+          View Details
+        </button>
         <div className="pinaddcomment">
           <button className="light" type="button">
             <span className="icaddcoment" />
           </button>
         </div>
       </div>
-      {/* <Popup
-          closeButton={true}
-          closeOnClick
-          longitude={info.coordinate[0]}
-          latitude={info.coordinate[1]}
-          style={{ paddingBottom: '16px' }}
-        >
-          <div className="bmodal">
-            <figure className="picture">
-              <img src="/public/images/img_market_s_001.png" alt="" />
-            </figure>
-            <div className="detailcard">
-              <div className="store">JABBO’S COMPACT MARKET</div>
-              <div className="attribute">
-                <span className="aitem">Fresh</span>
-                <span className="aitem">Quality</span>
-                <span className="aitem">Visible</span>
-              </div>
-              <div className="address">923 Ohio Ave, Clarksdale, MS</div>
-              <div className="phone">
-                <span className="icphone" />
-                <span className="number">(704) 969-9650</span>
-              </div>
-              <div className="pinaddcomment">
-                <button className="light" type="button">
-                  <span className="icaddc
-                            oment"
-                  />
-                </button>
-              </div>
-            </div>
-          </div>
-        </Popup> */}
     </div>
   );
 };
