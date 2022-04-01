d2241073ef4980ba9715ba7ca13d31c59c653363
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-04-01 15:51:54 -0400

    add html

---
+++ src/components/FeedbackForm.tsx
@@ -0,0 +1,224 @@
+export const FeedbackForm = () => {
+  console.log('FeedbackForm');
+  return (
+    <div className="modaluserfeedbck">
+      <div className="formpage close">
+        <div className="header">
+          <div className="backlink">
+            <button className="light" type="button">
+              <span className="icclose" />
+              <span className="txt">
+                Close
+              </span>
+            </button>
+          </div>
+          <h2 className="sectitle">User Feedback Form</h2>
+          <p className="secdescription">Have a location listed by completing the form below</p>
+        </div>
+        <div className="body">
+          <div className="group">
+            <div className="aleft">
+              <div className="sectiontitle">
+                Availability
+              </div>
+              <div className="item">
+                <div className="title grouped">
+                  <span className="number">1.</span>
+                  <span className="description">
+                    What type of fruits and vegetables are stocked at this location? Ple
+                    ase select categories with three or more items regularly stocked?
+                  </span>
+                </div>
+                <div className="ainput chk">
+                  <label className="chkwrap">
+                    Fresh (only continue if checks this)
+                    <input type="checkbox" />
+                    <span className="checkmark" />
+                  </label>
+                  <label className="chkwrap">
+                    Frozen
+                    <input type="checkbox" />
+                    <span className="checkmark" />
+                  </label>
+                  <label className="chkwrap">
+                    Canned
+                    <input type="checkbox" />
+                    <span className="checkmark" />
+                  </label>
+                </div>
+              </div>
+              <div className="sectiontitle">
+                Quality
+              </div>
+              <div className="item">
+                <div className="title grouped">
+                  <span className="number">2.</span>
+                  <span className="description">
+                    How would you describe the quality of the fresh fruits and
+                    vegetables at this location?
+                  </span>
+                </div>
+                <div className="ainput chk">
+                  <label className="chkwrap">
+                    Acceptable (peak condition, top quality, good color, fresh, firm, and clean)
+                    <input type="checkbox" />
+                    <span className="checkmark" />
+                  </label>
+                  <label className="chkwrap">
+                    Unacceptable (bruised, old looking, mushy, dry, overripe, dark sunken spots in Irregular patc
+                    hes or cracked or broken surfaces, signs of shriveling, mold or excessive softening)
+                    <input type="checkbox" />
+                    <span className="checkmark" />
+                  </label>
+                </div>
+              </div>
+              <div className="sectiontitle">
+                Visibility
+              </div>
+              <div className="item">
+                <div className="title grouped">
+                  <span className="number">3.</span>
+                  <span className="description">
+                    Are the fresh fruits and vegetables visible from the front of the
+                    store or before entering ?
+                  </span>
+                </div>
+                <div className="ainput chk">
+                  <label className="chkwrap">
+                    Yes
+                    <input type="checkbox" />
+                    <span className="checkmark" />
+                  </label>
+                  <label className="chkwrap">
+                    No
+                    <input type="checkbox" />
+                    <span className="checkmark" />
+                  </label>
+                </div>
+              </div>
+              <div className="sectiontitle">
+                Local
+              </div>
+              <div className="item">
+                <div className="title grouped">
+                  <span className="number">4.</span>
+                  <span className="description">
+                    Were any of the fruits and vegetables labeled as “locally
+                    grown”?
+                  </span>
+                </div>
+                <div className="ainput chk">
+                  <label className="chkwrap">
+                    Yes
+                    <input type="checkbox" />
+                    <span className="checkmark" />
+                  </label>
+                  <label className="chkwrap">
+                    No
+                    <input type="checkbox" />
+                    <span className="checkmark" />
+                  </label>
+                </div>
+              </div>
+              <div className="sectiontitle">
+                Meets Need
+              </div>
+              <div className="item">
+                <div className="title grouped">
+                  <span className="number">5.</span>
+                  <span className="description">
+                    Did the retailer or market have all of the fruits and vegetables
+                    you were looking for?
+                  </span>
+                </div>
+                <div className="ainput chk">
+                  <label className="chkwrap">
+                    Yes
+                    <input type="checkbox" />
+                    <span className="checkmark" />
+                  </label>
+                  <label className="chkwrap">
+                    No
+                    <input type="checkbox" />
+                    <span className="checkmark" />
+                  </label>
+                </div>
+              </div>
+              <div className="item">
+                <div className="title">
+                  <label>
+                    If no, please list what fruits and vegetables you would like this retailer to stock?
+                  </label>
+                </div>
+                <div className="ainput htxtarea">
+                  <textarea
+                    name="yth"
+                    id="yth2"
+                    placeholder="Your text here..."
+                  />
+                </div>
+              </div>
+              <div className="sectiontitle">
+                Photo Upload
+              </div>
+              <div className="item">
+                <div className="title">
+                  <label>
+                    (Please upload a photo where produce is stocked at this retailer)
+                  </label>
+                </div>
+                <div className="ainput upload">
+                  <div className="uploadarea">
+                    <div>Drag and drop files here</div>
+                    <div>or</div>
+                    <div>Browse on your device.</div>
+                  </div>
+                </div>
+              </div>
+              <div className="sectiontitle">
+                Contact
+              </div>
+              <div className="item">
+                <div className="title">
+                  <label>Email</label>
+                </div>
+                <div className="ainput">
+                  <input className="light" type="text" />
+                </div>
+              </div>
+              <div className="item">
+                <div className="title">
+                  <label>Phone Number</label>
+                </div>
+                <div className="ainput">
+                  <input className="light" type="text" />
+                </div>
+              </div>
+              <div className="item">
+                <div className="title">
+                  <label>Zip Code</label>
+                </div>
+                <div className="ainput">
+                  <input className="light" type="text" />
+                </div>
+              </div>
+              <div className="item">
+                <div className="ainput chk">
+                  <label className="chkwrap">
+                    Stay connected to your local retailers and your community! I agree to allow P
+                    HA and its affiliates to use my contact information for communication purposes.
+                    <input type="checkbox" />
+                    <span className="checkmark" />
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="aaction">
+            <button className="light" type="button">Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
