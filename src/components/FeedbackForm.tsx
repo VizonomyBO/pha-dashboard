c89a6e76663de255e164fd97a4268dc320ba3e85
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-04-04 19:29:34 -0400

    rename

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -15,7 +15,7 @@ export const FeedbackForm = (
     local,
     meets_need
   } = useIndividualFormState();
-  const setAbilityOptionsCheck = (type: string, checked: boolean, value: string) => {
+  const setAvailabityOptionsCheck = (type: string, checked: boolean, value: string) => {
     if (checked) {
       setIndividualForm(type, availability === '' ? value : `${availability}, ${value}`);
     } else {
@@ -59,7 +59,7 @@ export const FeedbackForm = (
                       type="checkbox"
                       onChange={
                         (e: React.FormEvent<HTMLInputElement>) => {
-                          setAbilityOptionsCheck(
+                          setAvailabityOptionsCheck(
                             TYPE_INDIVIDUAL_FORM.availability,
                             e.currentTarget.checked,
                             formConstants.AVAILABILITY.FRESH
@@ -75,7 +75,7 @@ export const FeedbackForm = (
                       type="checkbox"
                       onChange={
                         (e: React.FormEvent<HTMLInputElement>) => {
-                          setAbilityOptionsCheck(
+                          setAvailabityOptionsCheck(
                             TYPE_INDIVIDUAL_FORM.availability,
                             e.currentTarget.checked,
                             formConstants.AVAILABILITY.FROZEN
@@ -91,7 +91,7 @@ export const FeedbackForm = (
                       type="checkbox"
                       onChange={
                         (e: React.FormEvent<HTMLInputElement>) => {
-                          setAbilityOptionsCheck(
+                          setAvailabityOptionsCheck(
                             TYPE_INDIVIDUAL_FORM.availability,
                             e.currentTarget.checked,
                             formConstants.AVAILABILITY.CANNED
