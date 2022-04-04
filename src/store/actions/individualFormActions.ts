8b204546ccf5f62e7beee93e2a0ec3d20a9aff0c
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-04-04 14:48:47 -0400

    eddit action

--- src/store/actions/individualFormActions.ts
+++ src/store/actions/individualFormActions.ts
@@ -1,14 +1,11 @@
-import { Dispatch } from 'redux';
 import { MultimediaInterface } from '../../@types';
 import * as TYPES from '../types';
 
 export const setIndividualForm = (
   type: string,
   value: string | string[] | MultimediaInterface[]
-) => (dispatch: Dispatch) => {
-  dispatch({
-    type: TYPES.SET_INDIVIDUAL_FORM,
-    payload: type,
-    value
-  });
-};
+) => ({
+  type: TYPES.SET_INDIVIDUAL_FORM,
+  payload: type,
+  value
+});
