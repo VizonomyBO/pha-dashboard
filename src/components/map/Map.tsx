8a992ee82425e7c742187a8ea20e6c6cc6576186
JORGE <egrojmonroy@gmail.com>
2022-04-07 16:24:37 -0400

    rollback workflow and comment momentarily everything related to carto layer

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,14 +1,14 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { useState, useEffect } from 'react';
-import { CartoLayer, MAP_TYPES, setDefaultCredentials } from '@deck.gl/carto';
+// import { CartoLayer, MAP_TYPES, setDefaultCredentials } from '@deck.gl/carto';
 import { Layer, PickInfo } from 'deck.gl';
 import { deckDefaults } from './deckDefaults';
 import { DeckGLComponent } from './DeckGlComponent';
 import RenderTooltip from './RenderTooltip';
 import { useDeckMainMap } from '../../store/hooks/custom/useDeckMainMap';
 import { ViewStateChangeFn } from '../../@types';
-import { ICON_MAPPING } from '../../constants';
-import PinRed from './ic-pin-red.svg';
+// import { ICON_MAPPING } from '../../constants';
+// import PinRed from './ic-pin-red.svg';
 
 export const Map = () => {
   const [hoverInfo, setHoverInfo] = useState<PickInfo<Layer<unknown>[]>>();
@@ -30,25 +30,24 @@ export const Map = () => {
   };
 
   useEffect(() => {
-    setDefaultCredentials({
-      apiBaseUrl: 'https://gcp-us-east1.api.carto.com',
-      accessToken:
-        // eslint-disable-next-line
-        'eyJhbGciOiJIUzI1NiJ9.eyJhIjoiYWNfajl3eHQwbnoiLCJqdGkiOiI3ZjVkZTJhYiJ9.deIgT389U_YFodUSADFt5g6EccLWIJAbw1Ta0CraYxQ'
-    });
-    const cartLayer = new CartoLayer({
-      connection: 'carto_dw',
-      type: MAP_TYPES.QUERY,
-      data: 'select * from carto-dw-ac-j9wxt0nz.shared.pha_retailer_2',
-      pointType: 'icon',
-      pickable: true,
-      getIconSize: () => 29,
-      getIconColor: () => [255, 0, 0],
-      getIcon: () => 'marker',
-      iconMapping: ICON_MAPPING,
-      iconAtlas: PinRed
-    });
-    setLayers(cartLayer);
+    // setDefaultCredentials({
+    //   apiBaseUrl: 'https://gcp-us-east1.api.carto.com',
+    //   accessToken:
+    //     // eslint-disable-next-line
+    // });
+    // const cartLayer = new CartoLayer({
+    //   connection: 'carto_dw',
+    //   type: MAP_TYPES.QUERY,
+    //   data: 'select * from carto-dw-ac-j9wxt0nz.shared.pha_retailer_2',
+    //   pointType: 'icon',
+    //   pickable: true,
+    //   getIconSize: () => 29,
+    //   getIconColor: () => [255, 0, 0],
+    //   getIcon: () => 'marker',
+    //   iconMapping: ICON_MAPPING,
+    //   iconAtlas: PinRed
+    // });
+    setLayers([]);
 
     return () => {
       resetValues();
