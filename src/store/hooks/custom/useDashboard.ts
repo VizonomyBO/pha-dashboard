f13a1a2e0e12fa8fc945d48791dc0dd66dfb9a30
Lua <xdaddisxd@gmail.com>
2022-04-07 09:49:21 -0400

    move getquery params to utils

--- src/store/hooks/custom/useDashboard.ts
+++ src/store/hooks/custom/useDashboard.ts
@@ -1,25 +1,9 @@
 import { useEffect, useState } from 'react';
-import { QueryParams } from '../../../@types';
 import { PhaIndividual, PhaRetailer } from '../../../@types/database';
 import { defaultQueryParams } from '../../../constants/defaultValues';
 import { ENDPOINTS } from '../../../constants/url';
+import { getQueryParms } from '../../../utils/getQueryParams';
 import { webRequest } from '../../../utils/webRequest';
-
-const getQueryParms = (params: QueryParams) => {
-  let queryParams = '';
-  queryParams += `page=${params.page}`;
-  queryParams += `&limit=${params.limit}`;
-  if (params.search) {
-    queryParams += `&search=${params.search}`;
-  }
-  if (params.status) {
-    queryParams += `&status=${params.status}`;
-  }
-  if (params.dateRange) {
-    queryParams += `&dateRange=${params.dateRange}`;
-  }
-  return queryParams;
-};
 
 export const useDashboard = () => {
   const [params, setParams] = useState(defaultQueryParams);
