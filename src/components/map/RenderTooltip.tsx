8b7ef87a7c3a7f326aba0b551e71d3cd367aa59a
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-04-13 14:49:10 -0400

    add ToolTip Mobile

--- src/components/map/RenderTooltip.tsx
+++ src/components/map/RenderTooltip.tsx
@@ -1,13 +1,15 @@
 import { Layer, PickInfo } from 'deck.gl';
 import { ToolTip } from './ToolTip';
 import { PropertiesLayer } from '../../@types';
+import { ToolTipMobile } from './ToolTipMobile';
 
 export default function renderTooltip(info: PickInfo<Layer<unknown>[]> | undefined) {
+  const pageWidth = document.documentElement.scrollWidth;
   let toolTip = null;
   if (info?.object) {
     const { x, y, object } = info;
     const objectTypified = object as PropertiesLayer;
-    toolTip = ToolTip({ x, y, objectTypified });
+    toolTip = pageWidth === 375 ? ToolTipMobile({ x, y, objectTypified }) : ToolTip({ x, y, objectTypified });
   }
   return toolTip;
 }
