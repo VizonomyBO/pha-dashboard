58da087135e9d063a0732d87eb5fdd2ab4a3d099
jose <pachonjcl@gmail.com>
2022-04-15 20:09:03 -0400

    feat: display logout onhover

--- src/components/dashboard/DashboardNavbar.tsx
+++ src/components/dashboard/DashboardNavbar.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { authorizationManager } from '../../utils/authorizationManager';
 
 export const DashboardNavbar = () => {
+  const [showLogout, setShowLogout] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const profileRef = useRef<HTMLElement>(null);
   const navigate = useNavigate();
   const logout = () => {
     authorizationManager.logout();
     navigate('/home');
   };
+
+  useEffect(() => {
+    const mouseOverEvent = () => {
+      setShowLogout(true);
+    };
+    const mouseLeaveEvent = () => {
+      setTimeout(() => {
+        setShowLogout(false);
+      }, 1000);
+    };
+    const element = profileRef.current;
+    element?.addEventListener('mouseover', mouseOverEvent);
+    element?.addEventListener('mouseleave', mouseLeaveEvent);
+    return () => {
+      element?.removeEventListener('mouseover', mouseOverEvent);
+      element?.removeEventListener('mouseleave', mouseLeaveEvent);
+    };
+  }, []);
 
   return (
     <div className="navbararea">
@@ -27,15 +49,28 @@ export const DashboardNavbar = () => {
             <div className="userarea">
               <div className="profile dropdown">
                 <button className="light" type="button">
-                  <figure className="picture">
+                  <figure ref={profileRef} className="picture">
                     <img src="/images/profile.png" alt="" />
                   </figure>
                 </button>
-                <div className="dropdown-content logout">
+                {(showLogout || isHovered) && (
+                <div
+                  className="dropdown-content logout"
+                  onFocus={() => {
+                    console.debug('onFocus profile');
+                  }}
+                  onMouseOver={() => {
+                    setIsHovered(true);
+                  }}
+                  onMouseLeave={() => {
+                    setIsHovered(false);
+                  }}
+                >
                   <button className="light" type="button" onClick={() => logout()}>
                     Logout
                   </button>
                 </div>
+                )}
               </div>
             </div>
           </div>
