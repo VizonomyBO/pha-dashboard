a9161ba994578007f0e4a67414bcb27bbb09c0a5
jose <pachonjcl@gmail.com>
2022-04-18 01:27:29 -0400

    feat: add drag & drop feature

--- src/components/Attachment.tsx
+++ src/components/Attachment.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 import { useAttachmentBusiness } from '../store/hooks/custom/useAttachmentBusiness';
 import { MultimediFileInterface } from '../@types';
@@ -9,14 +10,60 @@ export const Attachment = ({ type, subType }: {type: string, subType?: string })
   const {
     filesSelected,
     multimedia,
-    removeFile
+    removeFile,
+    setMultimedia
   } = useAttachmentBusiness({ type, subType });
+  const [draggin, setDraggin] = useState(false);
   const { ref, width } = useWindowSize();
   const generateKey = (e: MultimediFileInterface, index: number) => `${e.file?.name || 'file'}-${index}-${type}`;
+
+  const handleDragIn = (e: React.DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+      setDraggin(true);
+    }
+  };
+  const handleDragOut = (e: React.DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDraggin(false);
+  };
+  const handleDrag = (e: React.DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDraggin(true);
+  };
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDraggin(false);
+    const newFiles = e.dataTransfer.files;
+    if (newFiles && newFiles.length > 0) {
+      const newObjects: Blob[] = [];
+      for (let i = 0; newFiles && i < newFiles.length; i += 1) {
+        newObjects.push(newFiles[i]);
+      }
+      setMultimedia([...multimedia, ...newObjects]);
+      e.dataTransfer.clearData();
+    }
+  };
+
   return (
     <div ref={ref}>
-      <div className="ainput upload">
-        <div className="uploadarea">
+      <div
+        className="ainput upload"
+        onDragEnter={handleDragIn}
+        onDragLeave={handleDragOut}
+        onDragOver={handleDrag}
+        onDrop={handleDrop}
+        style={{
+          border: draggin ? '4px solid #00BDE3' : 'none'
+        }}
+      >
+        <div
+          className="uploadarea"
+        >
           <div>Drag and drop files here</div>
           <div>or</div>
           <input
