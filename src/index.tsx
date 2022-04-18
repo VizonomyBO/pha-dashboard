d7f7539a94c48b47afb1cda88e48aec7fd369e6a
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-04-18 14:26:04 -0400

    edit meta

--- src/index.tsx
+++ src/index.tsx
@@ -29,7 +29,6 @@ import { theme } from './theme';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     <Provider store={store}>
       <React.StrictMode>
         <BrowserRouter>
