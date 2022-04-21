f1ade1b13f2a0b3085afe8ecac23d9f0793f3f2c
JORGE <egrojmonroy@gmail.com>
2022-04-21 15:48:54 -0400

    add USDA tooltip desktop and mobile

--- src/components/map/ToolTipUSDA.tsx
+++ src/components/map/ToolTipUSDA.tsx
@@ -22,15 +22,15 @@ export const ToolTipUSDA = (data: TooltipProps) => {
 
   return (
     <div
-      className={isMobile ? 'bmodal' : 'modal'}
+      className={isMobile ? 'bmodal usdamobile' : 'modal'}
       style={{ left: isMobile ? '' : x, top: isMobile ? '' : currentY }}
       ref={popupRef}
     >
       <div className={classNames('detailcard', isMobile ? 'detailCardOther' : '')}>
         <div className="store"><b>{getName(objectTypified).toUpperCase()}</b></div>
-        <div className="address">{getAddress(objectTypified)}</div>
-        <div className="store"> Payment Type</div>
-        <div className="address">{objectTypified?.properties?.snap_option ?? 'No payment type collected'}</div>
+        <div className="address marginbottom">{getAddress(objectTypified)}</div>
+        <div className="store marginbottompayment"> Payment Type</div>
+        <div className="payment">{objectTypified?.properties?.snap_option ?? 'No payment type collected'}</div>
       </div>
     </div>
   );
