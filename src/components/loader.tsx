71107083e58251a5a63a35a86a9ce4d0a8c5a643
JORGE <egrojmonroy@gmail.com>
2022-04-21 14:34:55 -0400

    fix colors markers usda osm and center name

---
+++ src/components/loader.tsx
@@ -0,0 +1,15 @@
+export const Loader = () => {
+  <div className="loader">
+    <ul>
+      <li id="a" />
+      <li id="b" />
+      <li id="c" />
+      <li id="d" />
+      <li id="e" />
+      <li id="f" />
+      <li id="g" />
+      <li id="h" />
+      <li id="i" />
+    </ul>
+  </div>;
+};
