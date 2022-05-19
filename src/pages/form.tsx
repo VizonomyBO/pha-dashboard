c4d572738240220a7f08381e7ecfef4e5b67698a
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-05-19 16:42:52 -0400

    fix Error name in form

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -39,7 +39,8 @@ export const Form = () => {
   const { name } = useParams();
   useEffect(() => {
     setBusinessDetails(TYPE_BUSINESS.NAME, name || '');
-  }, [name, setBusinessDetails]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   const {
     businessDetails,
     selectCategory,
