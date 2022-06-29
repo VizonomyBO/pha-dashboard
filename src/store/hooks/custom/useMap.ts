a8430b9fc2d2e74a5e7f80d678a773b86082f196
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-06-29 16:35:00 -0400

    add search in layers

--- src/store/hooks/custom/useMap.ts
+++ src/store/hooks/custom/useMap.ts
@@ -40,6 +40,7 @@ export const useMap = () => {
   const TRANSITION_DURATION = 1800;
   const TRANSITION_DURATION_GEOCODER = 500;
   const POSTCODE = 'postcode';
+  const { options } = useGeocoderState() || {};
   const PLACE = 'place';
   const ZOOM_POSTCODE = 12;
   const ZOOM_PLACE = 12;
@@ -50,12 +51,17 @@ export const useMap = () => {
   const [layers, setLayers] = useState<any>([]);
   const getLayers = useMemo(
     () => () => {
+      let valueSearch = '';
+      if (options.length > 0) {
+        valueSearch = options[0].text;
+      }
       const headers = webRequest.generateJSONHeader();
       const obj: CompletelyIntentionalAny = {
         categories: categoriesSelected,
         accesibility: accesibilities,
         dataSources,
-        badges: []
+        badges: [],
+        search: valueSearch,
       };
       if (superstarDateRange.length === 2) {
         obj.superstarDateRange = superstarDateRange;
@@ -78,7 +84,7 @@ export const useMap = () => {
         })
         .catch((err) => console.error(err));
     },
-    [categoriesSelected, accesibilities, dataSources, setCallFilters, superstarDateRange, verifiedDateRange]
+    [options, categoriesSelected, accesibilities, dataSources, superstarDateRange, verifiedDateRange, setCallFilters]
   );
 
   const getPinColor = (originTable: string) => {
